bccb2993497b82df25e43590b44db2e40a7bd0c3
Rep Graphics <charliebeckingham@hotmail.com>
2026-02-24 01:19:10 +0000

    Docker AGAIN!

--- www.js
+++ www.js
@@ -2,6 +2,26 @@ var networking = require("./lib/networking");
 var logging = require("./lib/logging");
 var config = require("./config");
 var retention = require("./lib/retention");
+var fs = require("fs");
+
+function ensureImageDirectories() {
+  var directories = [
+    config.directories.faces,
+    config.directories.helms,
+    config.directories.skins,
+    config.directories.renders,
+    config.directories.capes,
+  ];
+
+  for (var i = 0; i < directories.length; i += 1) {
+    var dirPath = directories[i];
+    try {
+      fs.mkdirSync(dirPath, { recursive: true });
+    } catch (err) {
+      logging.warn("failed to create image directory", dirPath, err && err.message);
+    }
+  }
+}
 
 process.on("uncaughtException", function(err) {
   logging.error("uncaughtException", err.stack || err.toString());
@@ -9,6 +29,7 @@ process.on("uncaughtException", function(err) {
 });
 
 setInterval(networking.resetCounter, 1000);
+ensureImageDirectories();
 retention.start();
 
 require("./lib/server.js").boot();
